36f3e20e7bd4a545a058ef979ad1edddda910062
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-22 22:14:29 +0300

    added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,7 @@
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<!-- <a href="/pokemon/pikachu">Pokemon [Pikachu]</a> -->
-<!-- <p>{ name }</p> -->
-<input type="text" bind:value={name}>
-<a href={"/pokemon/" + name.toLowerCase()}>Search</a>
+<div class="flex m-auto justify-center items-center">
+    <input type="text" bind:value={name}>
+    <a href={"/pokemon/" + name.toLowerCase()}>Search</a>
+</div>
--- src/routes/pokemon/[id]/+page.ts
+++ src/routes/pokemon/[id]/+page.ts
@@ -1,10 +1,8 @@
 import type { PageLoad } from "./$types"
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params, fetch }) => {
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
     const json = data.json()
 
-    return {
-        data: json
-    }
+    return { data: json }
 }
