06658c085adb463888dee690fe89c7442a5d73c8
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-22 11:13:27 +0300

    added search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
+<script lang="ts">
+
+    let name = 'Pikachu';
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/pokemon/pikachu">Pokemon [Pikachu]</a>
+<!-- <a href="/pokemon/pikachu">Pokemon [Pikachu]</a> -->
+<!-- <p>{ name }</p> -->
+<input type="text" bind:value={name}>
+<a href={"/pokemon/" + name.toLowerCase()}>Search</a>
