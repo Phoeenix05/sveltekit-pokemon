77efc25520dbd531bcb74cfbdce587d9761015a5
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-24 19:44:52 +0300

    added dynamic favicon and title

--- src/routes/pokemon/[id]/+page.svelte
+++ src/routes/pokemon/[id]/+page.svelte
@@ -6,6 +6,11 @@
 
 </script>
 
+<svelte:head>
+    <link rel="icon" type="image/png" href={data.data.sprites.front_default} id="favicon" />
+    <title>{ data.data.name.toLocaleUpperCase() }</title>
+</svelte:head>
+
 <code>
     <img src={data.data.sprites.front_default} alt={"sprite_" + data.data.name}>
     { data.data.name }
